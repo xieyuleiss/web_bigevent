5b055fba81730012e6a49fa99391072cbdcee68c
xieyuleiss <378286806@qq.com>
2020-06-28 19:49:49 +0800

    完成了登陆和注册功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,16 +1,17 @@
 $(function () {
     //点击“去注册账号”的链接
-    $('#link-reg').on('click', function () {
+    $('#link_reg').on('click', function () {
         $('.login-box').hide();
         $('.reg-box').show()
     })
-    $('#link-login').on('click', function () {
+    $('#link_login').on('click', function () {
         $('.reg-box').hide();
         $('.login-box').show()
     })
 
     //从layui中获取from对象
     var form = layui.form
+    var layer = layui.layer
 
     //通过form.verify()函数自定义效验规则
     form.verify({
@@ -34,18 +35,46 @@ $(function () {
 
     //监听注册表单的提交事件
     $('#form_reg').on('submit', function (e) {
+        //1.阻止默认的提交行为
         e.preventDefault()
-        $.post('http://ajax.frontend.itheima.net/api/reguser', {
+        //2.发起ajax的POST请求
+        var data = {
             username: $('#form_reg [name=username]').val(),
             password: $('#form_reg [name=password]').val(),
+        }
+
+        $.post('/api/reguser', data,
             function (res) {
                 if (res.status !== 0) {
                     return layer.msg(res.message)
                 }
-                console.log('注册成功');
+                layer.msg('注册成功,请登录');
+
+                //模拟人的点击行为
+                $('#link_login').click()
+            })
+    })
+
+
+    //监听登陆表单的提交事件
+    $('#form_login').submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            //快速获取表单事件
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登陆失败！')
+                }
+                layer.msg('登陆成功！')
+                // // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                // localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = './index.html'
             }
         })
     })
-
 
 })
